c765b441c224545d9bde29b8fe38d4d0464771fa
Samia Dahmouni <dsamia@live.fr>
2017-11-09 15:12:16 +0100

    update app

---
+++ app/controllers/concerns/doses_controller.rb
@@ -0,0 +1,37 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    # no need for app/views/doses/destroy.html.erb
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
+
+
