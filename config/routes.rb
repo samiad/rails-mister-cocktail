e8086607370cb85e5f7074ff8913aa3e96d6a685
Samia Dahmouni <dsamia@live.fr>
2017-11-10 12:32:19 +0100

    add picture to my cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+mount Attachinary::Engine => "/attachinary"
 root 'cocktails#index'
 resources :cocktails, only: [:show, :new, :create] do
   resources :doses, only: [:new, :create, :destroy], shallow: true
